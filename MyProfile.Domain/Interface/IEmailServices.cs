9fa6789dc76c78cab50a1f08aa76d29ca297b5e3
namch <namch@PC>
2025-05-06 14:11:49 +0700

    UpdateEmail

---
+++ MyProfile.Domain/Interface/IEmailServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProfile.Domain.Interface
+{
+    public interface IEmailServices
+    {
+        Task EmailAsysnc(string to, string subject, string body);
+    }
+}
