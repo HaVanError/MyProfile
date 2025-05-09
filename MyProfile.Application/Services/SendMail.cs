4cc89b3dcdd089e6e67bb1b082039c825cb89cbe
namch <namch@PC>
2025-05-09 21:37:04 +0700

    Revert "feat(email): allow HR to send email to users"
    This reverts commit 2cd34f666a1e931c300c901bed8de39931cfa5ac.

--- MyProfile.Application/Services/SendMail.cs
+++ MyProfile.Application/Services/SendMail.cs
@@ -19,17 +19,15 @@ namespace MyProfile.Application.Services
 
         public async Task SendMailAsysn(ContactRequest request)
         {
-          
-            byte[] fileBytes = request.FileContent;
-            //if (request.File != null)
-            //{
-            //    using (var ms = new MemoryStream())
-            //    {
-            //        //await request.File.CopyToAsync(ms);
-            //    //   await request.File.FileContent.CopyTo(ms);
-            //        fileBytes = ms.ToArray();
-            //    }
-            //}
+            byte[] fileBytes = null;
+            if (request.File != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    await request.File.CopyToAsync(ms);
+                    fileBytes = ms.ToArray();
+                }
+            }
 
             // Tạo request gửi email
             var emailRequest = new EmailFile
@@ -38,8 +36,8 @@ namespace MyProfile.Application.Services
                 Subject = request.Subject,
                 Message = request.Message,
                 FileContent = fileBytes,
-              //  FileName = request.File?.FileName,
-                ContentType = request.ContentType
+                FileName = request.File?.FileName,
+                ContentType = request.File?.ContentType
             };
 
             // Gọi tầng Infrastructure để gửi email
